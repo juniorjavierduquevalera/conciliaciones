c370b113b6bc4d9adfe0f20f9682bb611627fee1
DeboraMozart <deboramozart.santos@gmail.com>
2025-03-03 21:05:41 -0300

    input changes

--- src/components/auth/atomos/Input.tsx
+++ src/components/auth/atomos/Input.tsx
@@ -1,12 +1,25 @@
 interface InputProps {
-  type: 'text' | 'password' | 'email';
+  type: "text" | "password" | "email";
   placeholder: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: React.FC<InputProps> = ({ type, placeholder, value, onChange }) => {
-  return <input type={type} placeholder={placeholder} value={value} onChange={onChange} />;
+const Input: React.FC<InputProps> = ({
+  type,
+  placeholder,
+  value,
+  onChange,
+}) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    />
+  );
 };
 
 export default Input;
