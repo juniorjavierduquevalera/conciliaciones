1e6dc04c7e362ea1fcd7eedf92b9b5f1e9f57b2f
DeboraMozart <deboramozart.santos@gmail.com>
2025-04-04 22:34:16 -0300

    table estado de cuenta listo

--- src/app/pages/estado-de-cuenta/page.tsx
+++ src/app/pages/estado-de-cuenta/page.tsx
@@ -1,32 +1,66 @@
-// import ListaDeEstadoDeCuenta from "src/components/ui/organismo/ListaDeEstadoDeCuenta";
-
-// export default function page() {
-//   return (
-//     <>
-//      <ListaDeEstadoDeCuenta/> 
-//     </>
-//   )
-// }
-
 'use client'
 import { useEffect } from "react";
 import { useEstadoDeCuentaStore } from "../../../hooks/useEstadoDeCuentaStore";
+import DataTable, {TableColumn } from "react-data-table-component";
+import { interfaceEstadoDeCuenta } from "src/interfaces/interfaceEstadoDeCuenta";
 
 const EstadoDeCuentaPage = () => {
   const { estados, isLoading, fetchEstados } = useEstadoDeCuentaStore();
-
+  
   useEffect(() => {
     fetchEstados();
   }, []);
+  const columns: TableColumn<interfaceEstadoDeCuenta>[] = [
+    {
+      name: 'ID', selector: (row) => row.id, sortable: true
+    },
+    {
+      name: 'Fecha', selector: (row) => row.fecha, sortable: true
+    },
+    {
+      name: 'Referencia', selector: (row) => row.referencia, sortable: true
+    },
+    {
+      name: 'Concepto', selector: (row) => row.concepto, sortable: true
+    },
+    {
+      name: 'Cargo', selector: (row) => row.cargo, sortable: true
+    },
+    {
+      name: 'Abono', selector: (row) => row.abono, sortable: true
+    },
+    {
+      name: 'Saldo', selector: (row) => row.saldo, sortable: true
+    },
+    {
+      name: 'Codigo de operacion', selector: (row) => row.codigo_operacion, sortable: true
+    },
+    {
+      name: 'Tipo de operacion', selector: (row) => row.tipo_operacion, sortable: true
+    },
+    {
+      name: 'Fecha creacion', selector: (row) => row.fecha_creacion, sortable: true
+    },
+    {
+      name: 'Fecha actualizacion', selector: (row) => row.fecha_actualizacion, sortable: true
+    }
 
+  ]
   if (isLoading) {
     return <pre>Cargando...</pre>;
   }
 
+  const flattenedEstadoDeCuenta = estados.flat();
+
   return (
     <div>
       <h1>Lista de Estados de Cuenta</h1>
-      <pre>{JSON.stringify(estados, null, 2)}</pre>
+      <DataTable<interfaceEstadoDeCuenta>
+        columns={columns}
+        data={flattenedEstadoDeCuenta}
+        pagination
+        />
+      
     </div>
   );
 };
